690aeea24a218b4e51ba3375b46a4d546af0d19c
vandenjain <vandenjain0001@gmail.com>
2019-09-03 22:36:10 +0530

    First commit

---
+++ Cart/Cart.js
@@ -0,0 +1,38 @@
+function increaseQuantity(pro_id){
+    var itemQuantity = document.querySelector(".quantity ." + pro_id);
+    itemQuantity.innerHTML = Number(itemQuantity.innerHTML) + 1;
+    
+}
+
+function decreaseQuantity(pro_id){
+    var itemQuantity = document.querySelector(".quantity ." + pro_id);
+    if (Number(itemQuantity.innerHTML) - 1 >= 0){
+        itemQuantity.innerHTML = Number(itemQuantity.innerHTML) - 1;
+    }
+}
+function increaseQuantity(pro_id){
+    var itemQuantity = document.querySelector(".quantity ." + pro_id);
+    itemQuantity.innerHTML = Number(itemQuantity.innerHTML) + 1;
+    
+}
+
+function decreaseQuantity(pro_id){
+    var itemQuantity = document.querySelector(".quantity ." + pro_id);
+    if (Number(itemQuantity.innerHTML) - 1 >= 0){
+        itemQuantity.innerHTML = Number(itemQuantity.innerHTML) - 1;
+    }
+}
+function increaseQuantity(pro_id){
+    var itemQuantity = document.querySelector(".quantity ." + pro_id);
+    itemQuantity.innerHTML = Number(itemQuantity.innerHTML) + 1;
+    
+}
+
+function decreaseQuantity(pro_id){
+    var itemQuantity = document.querySelector(".quantity ." + pro_id);
+    if (Number(itemQuantity.innerHTML) - 1 >= 0){
+        itemQuantity.innerHTML = Number(itemQuantity.innerHTML) - 1;
+    }
+}
+
+
